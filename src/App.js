a2bffefce4f6d5dade933aace46a564f8d45293b
Yinan Li <gabriel.li5566@gmail.com>
2022-06-29 19:37:27 +0300

    Configed routes and path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <h1>App Component</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />}></Route>
+          <Route path="/home" element={<Layout />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+
+const Login = () => {
+  return (
+    <div>
+      Login Component
+      <Button type="primary">Button</Button>
+    </div>
+  )
+}
+
+export default Login
