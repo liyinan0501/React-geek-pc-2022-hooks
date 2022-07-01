e9a0c6ef4486be356e2f771f8091321b041cbd88
Yinan Li <gabriel.li5566@gmail.com>
2022-07-01 18:52:16 +0300

    Made a Channel component

---
+++ src/components/Channel/index.js
@@ -0,0 +1,35 @@
+import { Select } from 'antd'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getChannels } from 'store/actions'
+const { Option } = Select
+// 重点：props接收：value, onChange属性，配合antd的表单组件实现受控
+// value, onChange是Form.Item传入的，控制Select
+// props api设计
+function Channel({ value, onChange, width }) {
+  console.log(value, onChange, width)
+  const dispatch = useDispatch()
+  const channelList = useSelector((state) => state.channel)
+
+  useEffect(() => {
+    dispatch(getChannels())
+  }, [dispatch])
+
+  return (
+    <Select
+      placeholder="Select a channel"
+      value={value}
+      onChange={onChange}
+      style={{ width }}
+    >
+      {channelList.map((item) => (
+        // 说明：value绑定的值，就是下拉选中获取到的值
+        <Option key={item.id} value={item.id}>
+          {item.name}
+        </Option>
+      ))}
+    </Select>
+  )
+}
+
+export default Channel
--- src/pages/Publish/Publish.js
+++ src/pages/Publish/Publish.js
@@ -1,9 +1,13 @@
 import styles from './index.module.scss'
-import { Card, Breadcrumb } from 'antd'
+import { Card, Breadcrumb, Form, Button, Space, Input, message } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, DiffOutlined } from '@ant-design/icons'
+import Channel from 'components/Channel/index'
 
 const Publish = () => {
+  const onFinish = (values) => {
+    console.log(values)
+  }
   return (
     <div className={styles.root}>
       <Card
@@ -21,7 +25,44 @@ const Publish = () => {
             </Breadcrumb.Item>
           </Breadcrumb>
         }
-      ></Card>
+      >
+        <Form
+          labelCol={{ span: 2 }}
+          size="large"
+          onFinish={onFinish}
+          validateTrigger={['onBlur', 'onChange']}
+          // initialValues={{ channel_id: 4 }}
+        >
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: 'Title can not be empty.',
+              },
+            ]}
+          >
+            <Input
+              style={{ width: 400 }}
+              placeholder="Input article's title"
+            ></Input>
+          </Form.Item>
+          <Form.Item label="Channel" name="channel_id">
+            <Channel width={400} />
+          </Form.Item>
+          <Form.Item label="Cover"></Form.Item>
+          <Form.Item label="Content"></Form.Item>
+          <Form.Item wrapperCol={{ offset: 2 }}>
+            <Space>
+              <Button type="primary" htmlType="submit" size="large">
+                Publish
+              </Button>
+              <Button size="large">Save</Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
