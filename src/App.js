b460e459e0eb8ce619fb433cf21a53b7b442b339
Yinan Li <gabriel.li5566@gmail.com>
2022-07-03 00:58:48 +0300

    Compoleted whole project and Configed React.lazy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import Login from './pages/Login'
-import Layout from './pages/Layout'
+// import Login from './pages/Login'
+// import Layout from './pages/Layout'
 import NotFound from './pages/NotFound'
 
 import AuthRoute from 'components/AuthRoute'
@@ -13,27 +13,37 @@ import Publish from 'pages/Publish/Publish'
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import { history } from 'utils/history'
 
+import React, { Suspense } from 'react'
+const Login = React.lazy(() => import('./pages/Login'))
+const Layout = React.lazy(() => import('./pages/Layout'))
+
 function App() {
   return (
     <HistoryRouter history={history}>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />}></Route>
-          <Route
-            path="/home"
-            element={
-              <AuthRoute>
-                <Layout />
-              </AuthRoute>
-            }
-          >
-            <Route exact path="/home" element={<Home />}></Route>
-            <Route path="/home/article" element={<Article />}></Route>
-            <Route key="add" path="/home/publish" element={<Publish />}></Route>
-          </Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+        <Suspense fallback={<div>Loading</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />}></Route>
+            <Route
+              path="/home"
+              element={
+                <AuthRoute>
+                  <Layout />
+                </AuthRoute>
+              }
+            >
+              <Route exact path="/home" element={<Home />}></Route>
+              <Route path="/home/article" element={<Article />}></Route>
+              <Route
+                key="add"
+                path="/home/publish"
+                element={<Publish />}
+              ></Route>
+            </Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </Suspense>
       </div>
     </HistoryRouter>
   )
